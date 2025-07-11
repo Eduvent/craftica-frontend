de5d58966d3186c6583d09e60b9cdd34a7f05178
Ventura Chancafe, Eduardo Renato <eduventura7777@gmail.com>
2025-07-11 15:01:48 -0500

    fix: permitir imágenes externas de upload.wikimedia.org en next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+    domains: ['upload.wikimedia.org'],
+  },
   // Configuración para manejar Mixed Content de forma más permisiva
   async headers() {
     return [
